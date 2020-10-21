7f499e6e29ca699b8010689675f19003a94e7f74
Syun Hsieh <c0917099000@gmail.com>
2020-10-21 15:02:07 +0800

    將topBar 移動至app.vue

--- view/home.vue
+++ view/home.vue
@@ -1,23 +1,5 @@
 <template>
   <div class="homediv">
-    <div class="topbar">
-      <button class="searchBtn">
-        <svg
-          class="toolIcon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g data-name="Layer 2">
-            <g data-name="search">
-              <rect width="24" height="24" opacity="0" />
-              <path
-                d="M20.71 19.29l-3.4-3.39A7.92 7.92 0 0 0 19 11a8 8 0 1 0-8 8 7.92 7.92 0 0 0 4.9-1.69l3.39 3.4a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 11a6 6 0 1 1 6 6 6 6 0 0 1-6-6z"
-              />
-            </g>
-          </g>
-        </svg>
-      </button>
-    </div>
     <itemInViewportList
       ref="IVList"
       :that="this"
@@ -284,24 +266,5 @@ export default {
   width: 2.5rem;
   height: 2.5rem;
   fill: white;
-}
-
-.topbar {
-  position: fixed;
-  top: 0;
-  right: 1rem;
-  z-index: 100;
-}
-
-.searchBtn {
-  width: 3rem;
-  height: 3rem;
-  outline: none;
-  align-items: center;
-  border: none;
-  color: white;
-  font-size: 0.8rem;
-  background: transparent;
-  margin-top: 0.5rem;
 }
 </style>
